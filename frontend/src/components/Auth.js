bd2a953e27881c8bd4f872f8cb126d8b1d7129c3
Rahul-519 <rgavit519@gmail.com>
2022-11-24 14:49:01 +0530

    Frontend Updated

--- frontend/src/components/Auth.js
+++ frontend/src/components/Auth.js
@@ -1,9 +1,31 @@
+import { Box, Button, TextField, Typography } from '@mui/material';
 import React from 'react'
 
 const Auth = () => {
-  return (
-    <div>Auth</div>
-  )
-}
+  return(
+  <div>
+  <form>
+    <Box 
+    maxWidth={400}
+    display="flex" 
+    flexDirection={'column'} 
+    alignItems='center' 
+    justifyContent={'center'}
+    boxShadow="10px 10px 20px #ccc"
+    padding={3}
+    margin='auto'
+    marginTop={5}
+    borderRadius={5}
+    >
+      <Typography variant='h4' padding={3} textAlign="center">Login</Typography>
+      <TextField placeholder='Name' margin="normal" />
+      <TextField type={'email'} placeholder='Email' margin="normal" />
+      <TextField type={'password'} placeholder='Password' margin="normal" />
+      <Button variant='contained' sx={{borderRadius:3, marginTop: 3}} color='warning' >Submit</Button>
+      <Button sx={{borderRadius:3, marginTop: 3}} >Change To Signup</Button>
+    </Box>
+  </form>
+  </div>);
+};
 
-export default Auth
+export default Auth;
